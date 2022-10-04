20a1491ca4775f1f1829406cc331108bf097bfee
PearArdua <114182272+PearArdua@users.noreply.github.com>
2022-10-04 13:57:09 -0400

    Add files via upload
    Assignment submission, using if statements

---
+++ P3T1_McIntyre.py
@@ -0,0 +1,13 @@
+# CTI 110
+# P3T1 - Pass or Fail
+# Emma McIntyre
+# 10/4/22
+
+grade = int(input("Enter number grade: "))
+
+if grade >= 60:
+    print("Pass")
+else:
+    print("Fail")
+
+print("Done.")

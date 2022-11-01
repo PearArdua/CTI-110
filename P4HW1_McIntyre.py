2973d7e0f6a1f816cfea2f86f08beb2189ccca07
PearArdua <mcintyre4716@student.faytechcc.edu>
2022-11-01 14:00:17 -0400

    Update P4HW1_McIntyre.py
    Added lowest and highest grade output

--- P4HW1_McIntyre.py
+++ P4HW1_McIntyre.py
@@ -7,8 +7,13 @@
 # Add them to a list.
 
 grades = []
+
 for grade in range(6):
     grade = int(input("Enter grade: "))
     grades.append(grade)
     
 print(grades)
+# max(grades) and min (Grades)
+# to show lowest and highest in the list
+print("Lowest grade: ", min(grades))
+print("Highest grade: ", max(grades))
